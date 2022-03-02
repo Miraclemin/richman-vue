55e24fcdb24e1cf82d5d9de96d5b1ce705e08a34
zhanghandong@baidu.com <2429366607@qq.com>
2022-03-02 21:51:52 +0800

    add relationship

--- src/common/countryList.js
+++ src/common/countryList.js
@@ -1,2 +1,52 @@
-const countryList = ['中国', '日本', '美国', '俄罗斯', '比利时', '西班牙', '洪都拉斯'];
+// const countryList = ['中国', '日本', '美国', '俄罗斯', '比利时', '西班牙', '洪都拉斯'];
+const countryList = [
+  {
+    name: '中国',
+    impawn: 2000,
+    status: {
+      owner: '',
+      isImpawn: false,
+      stage: 1
+    },
+    stage1: 300,
+    stage2: 400,
+    stage3: 500,
+    stage4: 600,
+    stage5: 700,
+    roomCost: 100,
+    hotelCost: 200,
+  },
+  {
+    name: '日本',
+    impawn: 2000,
+    status: {
+      owner: '',
+      isImpawn: false,
+      stage: 1
+    },
+    stage1: 300,
+    stage2: 400,
+    stage3: 500,
+    stage4: 600,
+    stage5: 700,
+    roomCost: 100,
+    hotelCost: 200,
+  },
+  {
+    name: '英国',
+    impawn: 2000,
+    status: {
+      owner: '',
+      isImpawn: false,
+      stage: 1
+    },
+    stage1: 300,
+    stage2: 400,
+    stage3: 500,
+    stage4: 600,
+    stage5: 700,
+    roomCost: 100,
+    hotelCost: 200,
+  },
+];
 export default countryList
--- src/components/DepartmentCard.vue
+++ src/components/DepartmentCard.vue
@@ -19,11 +19,17 @@
             prop="debt"
             label="借贷（手动）"
             width="180">
+            <template slot-scope="scope">
+              <el-input-number clearable size="mini" v-model="scope.row.debt" :step = 50></el-input-number>
+            </template>
           </el-table-column>
           <el-table-column
             prop="owe"
             label="应还（自动计算）"
             width="180">
+            <template slot-scope="scope">
+              <p>{{Math.round(scope.row.debt * 1.1)}}</p>
+            </template>
           </el-table-column>
           <el-table-column
             prop="country"
@@ -39,16 +45,24 @@
             prop="random"
             label="随机事件（手动）"
             width="180">
+            <template slot-scope="scope">
+              <el-input clearable size="mini" v-model="scope.row.random"></el-input>
+              <el-button size="mini" @click="handleRandom(scope.$index)">计算</el-button>
+            </template>
           </el-table-column>
           <el-table-column
             prop="actualAsset"
             label="实际总资产（自动计算）"
             width="180">
+            <template slot-scope="scope">
+              <p>{{scope.row.cash - Math.round(scope.row.debt * 1.1)}}</p>
+            </template>
           </el-table-column>
         </el-table>
       </template>
     </div>
-    <p style="text-align: center; margin: 20px 0 20px">国家归属</p>
+<!--    <p style="text-align: center; margin: 20px 0 20px">国家归属</p>-->
+    <el-button style="text-align: center; margin: 20px 0 20px" size="mini" @click="handleReleaseForbidden">禁用/解除</el-button>
     <div style="text-align: center">
       <el-transfer
         style="text-align: left; display: inline-block; float: left"
@@ -62,6 +76,7 @@
       }"
         @change="handleChangeCountryBelongByA"
         :data="data">
+
       </el-transfer>
       <el-transfer
         style="text-align: left; display: inline-block; float: right"
@@ -126,9 +141,10 @@ export default {
       const cities = countryList
       cities.forEach((city, index) => {
         data.push({
-          label: city,
-          key: city + index,
-          disabled: false
+          label: city.name,
+          key: city.name + index,
+          disabled: false,
+          original: city
         });
       });
       return data;
@@ -141,28 +157,28 @@ export default {
       valueB: [-1],
       valueAImpawn: [],
       valueBImpawn: [],
-      text: 'hello',
+      disableAll: true,
       renderFunc(h, option) {
         return <span>{ option.label }</span>;
       },
       tableData: [{
         name: '张汉东',
         cash: 800,
-        debt: 100,
-        owe: 110,
-        country: 3,
-        countryInImpawn: 2,
+        debt: 0,
+        owe: 0,
+        country: 0,
+        countryInImpawn: 0,
         random: 0,
-        actualAsset: 690
+        actualAsset: 0
       }, {
         name: '王涵民',
         cash: 800,
-        debt: 100,
-        owe: 110,
-        country: 3,
-        countryInImpawn: 2,
+        debt: 0,
+        owe: 0,
+        country: 0,
+        countryInImpawn: 0,
         random: 0,
-        actualAsset: 690
+        actualAsset: 0
       }]
     };
   },
@@ -174,6 +190,11 @@ export default {
         this.dataAImpawn.push(JSON.parse(JSON.stringify(this.data[idx])))
         this.data[idx].disabled = true
       })
+      if (direction === 'right') {
+        this.tableData[0].country += movedKeys.length
+      } else if (direction === 'left') {
+        this.tableData[0].country -= movedKeys.length
+      }
     },
     handleChangeCountryBelongByB(value, direction, movedKeys) {
       movedKeys.forEach(obj => {
@@ -181,19 +202,57 @@ export default {
         this.dataBImpawn.push(JSON.parse(JSON.stringify(this.data[idx])))
         this.data[idx].disabled = true
       })
+      if (direction === 'right') {
+        this.tableData[1].country += movedKeys.length
+      } else if (direction === 'left') {
+        this.tableData[1].country -= movedKeys.length
+      }
     },
     handleChangeCountryImpawnByA(value, direction, movedKeys) {
-      movedKeys.forEach(obj => {
-        let idx = this.data.findIndex(cur => cur.key === obj)
-        this.data[idx].disabled = true
-      })
+      if (direction === 'right') {
+        movedKeys.forEach(obj => {
+          let idx = this.data.findIndex(cur => cur.key === obj)
+          this.tableData[0].cash += this.data[idx].original.impawn
+          this.tableData[0].actualAsset += this.data[idx].original.impawn
+        })
+        this.tableData[0].countryInImpawn += movedKeys.length
+      } else if (direction === 'left') {
+        movedKeys.forEach(obj => {
+          let idx = this.data.findIndex(cur => cur.key === obj)
+          this.tableData[0].cash -= this.data[idx].original.impawn
+          this.tableData[0].actualAsset -= this.data[idx].original.impawn
+        })
+        this.tableData[0].countryInImpawn -= movedKeys.length
+      }
     },
     handleChangeCountryImpawnByB(value, direction, movedKeys) {
-      movedKeys.forEach(obj => {
-        let idx = this.data.findIndex(cur => cur.key === obj)
-        this.data[idx].disabled = true
+      if (direction === 'right') {
+        movedKeys.forEach(obj => {
+          let idx = this.data.findIndex(cur => cur.key === obj)
+          this.tableData[1].cash += this.data[idx].original.impawn
+          this.tableData[1].actualAsset += this.data[idx].original.impawn
+        })
+        this.tableData[1].countryInImpawn += movedKeys.length
+      } else if (direction === 'left') {
+        movedKeys.forEach(obj => {
+          let idx = this.data.findIndex(cur => cur.key === obj)
+          this.tableData[1].cash -= this.data[idx].original.impawn
+          this.tableData[1].actualAsset -= this.data[idx].original.impawn
+        })
+        this.tableData[1].countryInImpawn -= movedKeys.length
+      }
+    },
+    handleRandom(index) {
+      // console.log(this.tableData[index].actualAsset)
+      // this.tableData[index].actualAsset = this.tableData[index].actualAsset + parseInt(this.tableData[index].random)
+      this.tableData[index].cash = this.tableData[index].cash + parseInt(this.tableData[index].random)
+    },
+    handleReleaseForbidden() {
+      this.disableAll = !this.disableAll
+      this.data.forEach(obj => {
+        obj.disabled = this.disableAll
       })
     }
-  }
+  },
 };
 </script>
